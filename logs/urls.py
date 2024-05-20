244ce9e013c99e4f24ef1ac14970e75d0dc7739e
Minh <luiminyan@gmail.com>
2024-05-20 12:27:25 +0200

    Added edit_entry

--- logs/urls.py
+++ logs/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path('topics/', views.topics, name='topics'),
     path('topics/<int:topic_id>/', views.topic, name='topic'),
     path('new_topic/', views.new_topic, name='new_topic'),
-    # take topic_id as an argument
+    # add new entry under a topic: take topic_id as an argument
     path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # edit entry
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
--- logs/views.py
+++ logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from logs.models import Topic
+from logs.models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 
@@ -60,3 +60,19 @@ def new_entry(request, topic_id):
     # Display a blank or invalid form.
     context = {'topic': topic, 'form': form, 'topic_id': topic_id}
     return render(request, 'new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != "POST":
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('logs:topic', topic_id=topic.id)
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'edit_entry.html', context)
